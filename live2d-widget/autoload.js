4e56fa974563a2f7eb143107581d05aaf727faef
Achuan-2 <achuan-2@outlook.com>
2021-10-07 12:52:57 +0800

    Site updated: 2021-10-07 12:52:56

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -34,9 +34,9 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "https://live2d.fghrsh.net/api/",
+			// apiPath: "https://live2d.fghrsh.net/api/",
 			// cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
-			// apiPath: "https://github.com/fghrsh/live2d_api/tree/1.0.1/"
+			cdnPath: "https://github.com/fghrsh/live2d_api/tree/1.0.1/"
 		});
 	});
 }
